daadb127e7ef179cc0f30117af2d1cee70c7480e
theruspm <rusyadmitrievich@mail.ru>
2025-09-17 16:34:29 +0700

    Initial commit: Nuxt 3 project setup

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: false },
+  // modules: ['@nuxtjs/i18n', 'nuxt-keen-slider'],
+  modules: [
+    "nuxt-icons",
+    "@nuxt/image",
+    "@vueuse/nuxt",
+    "@pinia/nuxt",
+    "nuxt-keen-slider",
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        less: {
+          additionalData: `@import "@/assets/less/main.less";`,
+        },
+      },
+    },
+  },
+  css: ["~/assets/css/main.css", "~/assets/css/reset.css"],
+  ssr: false,
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap",
+        },
+      ],
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+  <p class="hello">Каталог курсов</p>
+</template>
+<style scoped lang="less">
+.hello {
+  font-family: "Nekst";
+  font-size: 32px;
+}
+</style>
